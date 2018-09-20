c30596b30582c03969a2b0f72cb528138d4a7a22
RequiemRain828 <43284091+RequiemRain828@users.noreply.github.com>
2018-09-20 08:35:21 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock.js'
+
+let time = new Date().toLocaleString();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Hello World!
+        </p>
+        <Clock /> 
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+let time = new Date().toLocaleString();
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			time: new Date().toLocaleString()
+		};
+	}
+	componentDidMount() {
+		this.intervalID = setInterval(
+			() => this.updateClock(),
+			1000
+			);
+	}
+	componentWillMount() {
+		clearInterval(this.intervalID);
+	}
+	updateClock() {
+		this.setState({
+			time: new Date().toLocaleString()
+		});
+	}
+	render() {
+		return (
+			<p>
+			The time is {this.state.time};
+			</p>
+			);
+	}
+}
+
+export default Clock;
